3fe92fdb7f8365d431919beab1b660ebe96073dd
RareMashiro <symon.belmont2015@yandex.ru>
2024-03-22 18:46:25 +0300

    An issue with a sum counter was resolved!

--- src/components/cartItem/component.jsx
+++ src/components/cartItem/component.jsx
@@ -4,18 +4,26 @@ import { deleteAll } from "../../redux/ui/cart"
 import { Counter } from "../counter/component";
 import { Button } from "../button/component"
 import styles from './styles.module.scss';
+import { useContext } from "react";
+import { ProductSumContext } from "../../../contexts/productSum";
 
 
-export const CartItem = ({ product, productId }) => {
+export const CartItem = ({ product, productId, currAmount }) => {
     const dispatch = useDispatch();
-    
+    const { sum, setSum } = useContext(ProductSumContext);
+
     return (
         <li>
             <div className={styles.items}>
                 <div className={styles.title}>{product.title}</div>
                 <div className={styles.control}>
                     <Counter product={product}/>
-                    <Button onClick={() => dispatch(deleteAll(productId))}>
+                    <Button onClick={() => 
+                        {
+                            dispatch(deleteAll(productId));
+                            setSum(sum - product.price * currAmount);
+                        }
+                    }>
                             <img className={styles.bucket} 
                                 src='https://img1.pngindir.com/20180520/owg/kisspng-rubbish-bins-waste-paper-baskets-recycling-bin-c-5b01bb379fbdc4.9342500115268401196543.jpg'
                             >
--- src/components/cartItem/container.jsx
+++ src/components/cartItem/container.jsx
@@ -2,15 +2,17 @@
 import { useSelector } from "react-redux";
 import { CartItem } from "./component"
 import { selectProductById } from "../../redux/entities/product";
+import { selectProductAmountById } from "../../redux/ui/cart";
 
 export const CartItemContainer = ({ productId }) => {
     const product = useSelector((state) => selectProductById(state, productId));
+    const currAmount = useSelector(state => selectProductAmountById(state, productId));
     
     if(!product) {
         return <>Loading...</>
     }
 
     return (
-        <CartItem product={product} productId={productId}/>
+        <CartItem product={product} productId={productId} currAmount={currAmount}/>
     )
 }
